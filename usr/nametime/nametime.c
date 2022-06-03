cc843964e129d7771115082880ebb780bb9cc559
liuzikai <zikai.liu@outlook.com>
2022-06-03 20:13:37 +0200

    Update nametime

--- usr/nametime/nametime.c
+++ usr/nametime/nametime.c
@@ -4,6 +4,8 @@
 #include <aos/systime.h>
 #include <aos/aos_rpc.h>
 
+static uint8_t response_buf[4096];
+
 static void time_handler(void *st, void *message, size_t bytes, void **response, size_t *response_bytes, struct capref tx_cap, struct capref *rx_cap)
 {
 	systime_t stop = systime_now();
@@ -11,8 +13,8 @@ static void time_handler(void *st, void *message, size_t bytes, void **response,
 	assert(bytes >= sizeof(systime_t));
 	
 	systime_t start = *(systime_t*)message;
-	*response = malloc(sizeof(uint64_t));
-	*response_bytes = sizeof(uint64_t);
+	*response = response_buf;
+	*response_bytes = bytes;
 	
 	*(uint64_t*)*response = systime_to_us(stop - start);
 }
@@ -38,22 +40,31 @@ int main(int argc, char** argv) {
 			printf("cannot connect to server, err: %s\n", err_getstring(err));
 			return 1;
 		}
-		
-		size_t size;
-		uint64_t *one_way;
-		uint64_t two_way;
-		systime_t start = systime_now();
-		err = nameservice_rpc(ntchan, &start, sizeof(systime_t), (void**)&one_way, &size, NULL_CAP, NULL_CAP);
-		systime_t stop = systime_now();
-		two_way = systime_to_us(stop - start);
-		
-		if (err_is_fail(err)) {
-			printf("cannot time round trip, err: %s\n", err_getstring(err));
-		} else {
-			printf("One Way: %ldus, Two way: %ldus\n", *one_way, two_way);
-		}
-		
-		free(one_way);
+
+        systime_t *buf = calloc(4096, 1);
+
+        for (size_t s = 64; s < 4096; s += 64) {
+            for (int i = 0; i < 5; ++i) {
+                size_t size;
+                uint64_t *one_way;
+                uint64_t two_way;
+                systime_t start = systime_now();
+                *buf = start;
+
+                err = nameservice_rpc(ntchan, buf, s, (void**)&one_way, &size, NULL_CAP, NULL_CAP);
+                systime_t stop = systime_now();
+                two_way = systime_to_us(stop - start);
+
+                if (err_is_fail(err)) {
+                    printf("cannot time round trip, err: %s\n", err_getstring(err));
+                } else {
+                    printf("%lu, %d, One Way: %ldus, Two way: %ldus\n", s, i, *one_way, two_way);
+                }
+
+                free(one_way);
+            }
+        }
+
 	}
 	
 	return 0;
